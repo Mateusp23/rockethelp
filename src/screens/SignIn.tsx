9175aecbbfde30b8bd61f309122e1c90854aca19
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-07-24 19:09:26 -0300

    adicionando lib de icons e implementando inputs

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,12 @@
-import { VStack, Heading } from 'native-base';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native'; 
 
 import Logo from '../assets/logo_primary.svg';
+import { Input } from '../components/Input';
 
 export function SignIn() {
+  const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
@@ -11,6 +15,17 @@ export function SignIn() {
         Acesse sua conta
       </Heading>
 
+      <Input 
+        placeholder="E-mail" 
+        mb={4}
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+      />
+      <Input 
+        placeholder="Senha" 
+        mb={4}
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+      />
     </VStack>
   );
 }
