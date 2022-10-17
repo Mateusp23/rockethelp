287de4e32713339eb55d3105e7d587b43046a821
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-10-17 16:17:59 -0300

    realizando logout do app

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,17 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
-import { VStack, HStack, IconButton, useTheme, Text, Heading, FlatList, Center } from 'native-base';
+import {
+  VStack,
+  HStack,
+  IconButton,
+  useTheme,
+  Text,
+  Heading,
+  FlatList,
+  Center,
+} from 'native-base';
 import { SignOut } from 'phosphor-react-native';
 import { ChatTeardropText } from 'phosphor-react-native';
 
@@ -10,7 +21,9 @@ import { Button } from '../components/Button';
 import { Order, OrderProps } from '../components/Order';
 
 export function Home() {
-  const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+  const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>(
+    'open'
+  );
   const [orders, setOrders] = useState<OrderProps[]>([
     {
       id: '1',
@@ -31,6 +44,15 @@ export function Home() {
     navigation.navigate('details', { orderId });
   }
 
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch((error) => {
+        console.log(error);
+        return Alert.alert('Sair', 'Não foi possível sair.');
+      });
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -41,38 +63,35 @@ export function Home() {
         pt={12}
         pb={5}
         px={6}
-      > 
+      >
         <Logo />
-        <IconButton 
+        <IconButton
           icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
         />
       </HStack>
 
       <VStack flex={1} px={6}>
-        <HStack 
-          w="full" 
-          mt={8} 
-          mb={4} 
+        <HStack
+          w="full"
+          mt={8}
+          mb={4}
           justifyContent="space-between"
           alignItems="center"
         >
-          <Heading color="gray.100">
-            Solicitações
-          </Heading>
+          <Heading color="gray.100">Solicitações</Heading>
 
-          <Text color="gray.200">
-            {orders.length}
-          </Text>
+          <Text color="gray.200">{orders.length}</Text>
         </HStack>
 
         <HStack space={3} mb={8}>
-          <Filter 
+          <Filter
             type="open"
             title="Em andamento"
             onPress={() => setStatusSelected('open')}
             isActive={statusSelected === 'open'}
           />
-          <Filter 
+          <Filter
             type="closed"
             title="Finalizados"
             onPress={() => setStatusSelected('closed')}
@@ -80,18 +99,21 @@ export function Home() {
           />
         </HStack>
 
-        <FlatList 
+        <FlatList
           data={orders}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 100 }}
           ListEmptyComponent={() => (
             <Center>
-              <ChatTeardropText color={colors.gray[300]} size={40}/>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
               <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
                 Você ainda não possui {'\n'}
-                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                solicitações{' '}
+                {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
               </Text>
             </Center>
           )}
